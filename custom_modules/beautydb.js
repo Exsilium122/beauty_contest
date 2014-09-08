0960458a5fa82bbf100d3759b463b86103707d23
alex <alex+github@gralak.org>
2014-09-08 17:10:57 +0200

    added configuartion file

--- custom_modules/beautydb.js
+++ custom_modules/beautydb.js
@@ -1,3 +1,4 @@
 var mongo = require('mongoskin');
+var config = require('../config/database.json');
 exports.ObjectID = mongo.ObjectID;
-exports.db = mongo.db("mongodb://localhost:27017/beauty", {native_parser:true});
+exports.db = mongo.db(config.beautydb, {native_parser:true});
